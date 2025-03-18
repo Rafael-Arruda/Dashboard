9479601427ad9455c0ce3614e1ab50b13091ed77
Rafael <rafael.silva@opovo.com.br>
2025-03-18 10:19:47 -0300

    feat: Header da Landing page do sistema

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,49 @@
 import React from "react"
 
-function Home () {
+import Link from "next/link";
+import Image from "next/image";
+
+import Logo from "../../../dashboard/public/logo.svg"
+
+function Home() {
   return (
     <>
-      Página Home
+      {/* Header */}
+      <header className="bg-white border-b">
+        <div className="w-full max-w-[1320px] m-auto px-3">
+          <div className="flex items-center justify-between h-20">
+            <div>
+              <Link href="/">
+                <Image
+                  src={Logo}
+                  alt="Logo do site"
+                  width={154}
+                  height={32}
+                  priority
+                />
+              </Link>
+            </div>
+            <nav>
+              <ul className="flex gap-4">
+                <li className="hover:underline">
+                  <Link href="/dashboard">Dashboard</Link>
+                </li>
+                <li className="hover:underline">
+                  <Link href="/dashboard/table">Tabelas</Link>
+                </li>
+                <li className="hover:underline">
+                  <Link href="/dashboard/contato">Contato</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </header>
+
+      {/* Main */}
+      <main>
+        Aqui vai ter o conteúdo da Landing page do sistema.
+      </main>
     </>
   )
 }
